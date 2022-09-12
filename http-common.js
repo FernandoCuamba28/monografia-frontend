cd0ce7310f5a5a468d3292180a4469f891859a61
FernandoCuamba28 <email>
2022-09-12 14:56:21 +0200

    ok

--- http-common.js
+++ http-common.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-
 const http = axios.create({
     // baseURL:'http://localhost:3333/api',
     // baseURL:'https://atendimentoplus.herokuapp.com/api/'
     baseURL:'  https://controle-de-horas.herokuapp.com/api'
-  
-
 });
-
 export default http;
+
+
